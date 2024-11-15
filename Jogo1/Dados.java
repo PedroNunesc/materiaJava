c28f91db54f4e9a773cf737bc1ac2218fd42e0fc
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2024-11-15 20:48:01 -0300

    Add files via upload

---
+++ Jogo1/Dados.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.Jogo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Dados {
+    public static int numeroAleatorio() {
+        return (int) (Math.random() * 6 + 1);
+    }
+}
--- Jogo1/Personagem.java
+++ Jogo1/Personagem.java
@@ -1,31 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Jogo1;
+package com.example.Jogo1;
 
 /**
  *
- * @author PEDROHENRIQUENUNESCA
+ * @author Usuario
  */
-public class Personagem {
-    public String Nome;
-    public int Vida;
-    public int Ataque;
-    
-    public Personagem(String Nome, int Vida, int Ataque){
-        this.Nome = Nome;
-        this.Vida = Vida;
-        this.Ataque = Ataque;
-    }
-    
-    public void Atacar(Personagem oponente){
-        oponente.Vida -= this.Ataque;
-        System.out.println(Nome + " atacou o " + oponente.Nome + " e causou " + this.Ataque + " de dano.");
-    }
-    
-    public boolean estaVivo() {
-        return Vida > 0;
-    }
-    
+public interface Personagem {
+    public int Dano();
+    public int Vida();
+    public int Mana();
+    public int Agilidade();
 }
