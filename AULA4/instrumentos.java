cf8af04174ab776ef0c06cbe3ae0238928bf62ca
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2024-11-14 21:13:54 -0300

    Add files via upload

---
+++ AULA4/instrumentos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package AULA4;
+
+/**
+ *
+ * @author PEDROHENRIQUENUNESCA
+ */
+public interface instrumentos {
+       public void tocar();
+       public void afinar();
+}
