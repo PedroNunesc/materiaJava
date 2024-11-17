0d00cb13302038566292d8f4d12bd7a581ad1ce3
PedroNunes <162641504+PedroNunesc@users.noreply.github.com>
2024-11-16 22:48:32 -0300

    Add files via upload

---
+++ Jogo1/Jogo1/Personagem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.Jogo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Personagem {
+    public int Dano();
+    public int Vida();
+    public int Mana();
+    public int Agilidade();
+         
+    public int Atirar();
+    
+    public int LancarGranada();
+    
+    public int UsarBandagem();
+    
+    public int Fugir();
+    
+}
